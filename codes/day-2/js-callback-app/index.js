8af6a7a7536eb7e09008e6d5694e9fb265c8fae5
Joydip <joydip.mondal@gmail.com>
2022-07-19 10:31:00 +0530

    day-2: callback update

---
+++ codes/day-2/js-callback-app/index.js
@@ -0,0 +1,31 @@
+function filterValues(arr, logicForFiltration) {
+    var result = []
+    for (var i = 0; i < arr.length; i++) {
+        var element = arr[i]
+        var isTrue = logicForFiltration(element)
+
+        if (isTrue)
+            result.push(element)
+    }
+    return result
+}
+
+function isEven(num) {
+    return num % 2 === 0 ? true : false
+}
+function isOdd(num) {
+    return num % 2 !== 0 ? true : false
+}
+var numbers = [0, 2, 1, 3, 5, 4, 7, 9, 6, 8]
+var evenNumbers = filterValues(numbers, isEven)
+console.log(evenNumbers)
+
+var oddNumbers = filterValues(numbers, isOdd)
+console.log(oddNumbers)
+
+function substringExists(name) {
+    return name.indexOf('a') !== -1 ? true : false
+}
+var names = ['reetu', 'abhinandan', 'sonal', 'ananya', 'saurav']
+var resultantNames = filterValues(names, substringExists)
+console.log(resultantNames)
